f730e7955b79969f679c55145dede3cf0d022db3
stvake <stvakemail@gmail.com>
2024-10-26 18:08:36 +0300

    Lab 3 - code doc

--- src/main/java/org/labs/lab3/Lab3.java
+++ src/main/java/org/labs/lab3/Lab3.java
@@ -3,7 +3,20 @@ package org.labs.lab3;
 import java.util.Arrays;
 import java.util.Comparator;
 
+/**
+ * Головний клас для демонстрації роботи з масивом об'єктів класу SeaBoat.
+ * Створює масив, сортує його за вказаними полями та виконує пошук об'єкта.
+ * <p>
+ * Реалізація варіанту 3:
+ * Визначити клас морський човен, який складається як мінімум з 5-и полів.
+ */
 public class Lab3 {
+    /**
+     * Головний метод програми. Створює масив човнів, сортує його за довжиною (зростанням)
+     * і за роком побудови (спаданням). Також здійснює пошук заданого об'єкта у масиві.
+     *
+     * @param args аргументи командного рядка
+     */
     public static void main(String[] args) {
         SeaBoat[] boats = {
                 new SeaBoat("WaveRider", 30, 2015, 41, "Blue"),
@@ -14,6 +27,7 @@ public class Lab3 {
                 new SeaBoat("Voyager", 40, 2015, 38, "White")
         };
 
+        // Сортування за довжиною за зростанням і за роком побудови за спаданням
         Arrays.sort(
                 boats,
                 Comparator
@@ -25,11 +39,13 @@ public class Lab3 {
                         )
         );
 
+        // Виводимо відсортовані човни
         System.out.println("Sorted boats:");
         for (SeaBoat boat : boats) {
             System.out.println(boat);
         }
 
+        // Пошук заданого човна
         SeaBoat targetBoat = new SeaBoat("Speedy-II", 25, 2022, 50, "Yellow");
         boolean found = Arrays.asList(boats).contains(targetBoat);
         if (found) {
--- src/main/java/org/labs/lab3/SeaBoat.java
+++ src/main/java/org/labs/lab3/SeaBoat.java
@@ -1,5 +1,9 @@
 package org.labs.lab3;
 
+/**
+ * Клас Boat представляє морський човен з основними характеристиками, такими як ім'я, довжина, рік побудови,
+ * максимальна швидкість та колір.
+ */
 public class SeaBoat {
     private final String name;
     private final int length;
@@ -7,6 +11,15 @@ public class SeaBoat {
     private final int maxSpeed;
     private final String color;
 
+    /**
+     * Конструктор для створення об'єкта морського човна з заданими параметрами.
+     *
+     * @param name      назва човна
+     * @param length    довжина човна
+     * @param builtYear рік побудови човна
+     * @param maxSpeed  максимальна швидкість човна
+     * @param color     колір човна
+     */
     public SeaBoat(String name, int length, int builtYear, int maxSpeed, String color) {
         this.name = name;
         this.length = length;
@@ -15,26 +28,57 @@ public class SeaBoat {
         this.color = color;
     }
 
+    /**
+     * Повертає назву човна.
+     *
+     * @return назва човна
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Повертає довжину човна.
+     *
+     * @return довжина човна
+     */
     public int getLength() {
         return length;
     }
 
+    /**
+     * Повертає рік побудови човна.
+     *
+     * @return рік побудови човна
+     */
     public int getBuiltYear() {
         return builtYear;
     }
 
+    /**
+     * Повертає максимальну швидкість човна.
+     *
+     * @return максимальна швидкість човна
+     */
     public double getMaxSpeed() {
         return maxSpeed;
     }
 
+    /**
+     * Повертає колір човна.
+     *
+     * @return колір човна
+     */
     public String getColor() {
         return color;
     }
 
+    /**
+     * Порівнює об'єкт з іншим об'єктом для перевірки на рівність.
+     *
+     * @param obj об'єкт, з яким порівнюється поточний човен
+     * @return true, якщо об'єкти однакові; false - якщо ні
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
@@ -47,6 +91,11 @@ public class SeaBoat {
                 color.equals(boat.color);
     }
 
+    /**
+     * Повертає рядкове представлення об'єкта човна.
+     *
+     * @return рядок, що описує човен
+     */
     @Override
     public String toString() {
         return "Boat {" +
