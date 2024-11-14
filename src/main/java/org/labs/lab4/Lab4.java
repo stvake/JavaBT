bc0140de72aab4a1f96be24daedae4c8063c03a6
stvake <stvakemail@gmail.com>
2024-11-14 13:49:10 +0200

    Lab 4 - added text input via console

--- src/main/java/org/labs/lab4/Lab4.java
+++ src/main/java/org/labs/lab4/Lab4.java
@@ -1,5 +1,7 @@
 package org.labs.lab4;
 
+import java.util.Scanner;
+
 /**
  * Завдання: Відсортувати слова заданого тексту, що починаються з голосних літер, за другою літерою.
  * Замінити послідовність табуляцій та пробілів одним пробілом.
@@ -10,15 +12,16 @@ public class Lab4 {
      * @param args аргументи командного рядка
      */
     public static void main(String[] args) {
-        StringBuilder inputText = new StringBuilder(
-                "Ананас Кабан,    осінь Качка. Їжак     уява, іграшка. " +
-                        "Курка        енциклопедія, Тигр одяг Тигр2, ялинка."
-        );
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введіть текст:");
+        String inputText = scanner.nextLine();
 
-        Text text = new Text(inputText);
+        Text text = new Text(new StringBuilder(inputText));
         text.parseText();
         text.sortWordsStartingWithVowel();
 
         System.out.println("Відсортований текст:\n" + text);
     }
 }
+
+// Ананас Кабан,    осінь Качка. Їжак     уява, іграшка. Курка        енциклопедія, Тигр одяг Тигр2, ялинка.
